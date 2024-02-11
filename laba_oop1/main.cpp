69f960798dcf6f37b07a29fdf4647ef3917a5bdd
sonyaspin <mechieva.sofia@gmail.com>
2024-02-11 16:29:52 +0300

    инициализация строки

--- laba_oop1/main.cpp
+++ laba_oop1/main.cpp
@@ -1,13 +1,25 @@
-#include "Mystring.hpp"
+#include "MyString.hpp"
 #include <string>
 
 int main()
 {
-    MyString a1("short");
-    int cap1 = a1.capacity();
-    std::cout << cap1 << std::endl;
-    a1 = "loooooooooooooong";
-    a1.insert(0, "__");
-    std::cout << a1 << " " << a1.size() << " " << a1.capacity();
-    return 0;
+    MyString a1;
+
+    std::cout << a1 << " " << a1.size() << " " <<a1.capacity();
+    
+    MyString a2("Hello world!");
+    std::cout << a2 << " " << a2.size() << " " << a2.capacity() << std::endl;
+    MyString a3({ 'h','e','l','l','o' });
+    std::cout << a3 << " " << a3.size() << " " << a3.capacity() << std::endl;
+    std::string s = "hello";
+    MyString a4(s);
+    std::cout << a4 << " " << a4.size() << " " << a4.capacity() << std::endl;
+    //MyString a5("hello", 4);
+   // std::cout << a5 << " " << a5.size() << " " << a5.capacity() << std::endl;
+    MyString a6(5, '!');
+    std::cout << a6 << " " << a6.size() << " " << a6.capacity() << std::endl;
+    MyString a7("hello");
+    MyString a8(a7);
+    std::cout << a8 << " " << a8.size() << " " << a8.capacity() << std::endl;
+
 }
